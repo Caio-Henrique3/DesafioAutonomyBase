dbd48f44c5bdce02885a5091d97b70e05bf59d0d
Caio Henrique <caioaranha@hotmail.com>
2025-07-16 21:24:40 -0300

    feat: implementation of initial project structure

---
+++ Backend/src/main/java/br/com/autonomybase/AutonomyBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.autonomybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutonomyBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutonomyBaseApplication.class, args);
+	}
+
+}
